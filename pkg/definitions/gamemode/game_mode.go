4b5f91a0c7ae1ba26af2c9aa54d7f5b961b43203
sauerbraten <willing.alexander@googlemail.com>
2019-03-20 22:36:44 +0100

    add gamemode.Parse function

--- pkg/definitions/gamemode/game_mode.go
+++ pkg/definitions/gamemode/game_mode.go
@@ -1,8 +1,12 @@
 package gamemode
 
-import "strconv"
+import (
+	"strings"
+)
 
 type ID int32
+
+const Unknown ID = -1
 
 const (
 	FFA ID = iota
@@ -79,7 +83,60 @@ func (gm ID) String() string {
 	case EfficCollect:
 		return "effic collect"
 	default:
-		return strconv.Itoa(int(gm))
+		return "unknown"
+	}
+}
+
+func Parse(s string) ID {
+	switch strings.ToLower(s) {
+	case "ffa", "free for all":
+		return FFA
+	case "coop", "coop edit", "edit":
+		return CoopEdit
+	case "teamplay":
+		return Teamplay
+	case "i", "insta", "instagib":
+		return Insta
+	case "i team", "iteam", "insta team", "instateam", "instagib team", "instagibteam":
+		return InstaTeam
+	case "e", "effic", "efficiency":
+		return Effic
+	case "e team", "eteam", "effic team", "efficteam", "efficiency team", "efficiencyteam":
+		return EfficTeam
+	case "tac", "tactics":
+		return Tactics
+	case "tac team", "tacteam", "tactics team", "tacticsteam":
+		return TacticsTeam
+	case "cap", "capture":
+		return Capture
+	case "rcap", "regen", "regen cap", "regen capture":
+		return RegenCapture
+	case "ctf", "capture the flag":
+		return CTF
+	case "i ctf", "ictf", "insta ctf", "instactf", "instagib ctf", "instagibctf", "instagib capture the flag":
+		return InstaCTF
+	case "protect":
+		return Protect
+	case "i protect", "iprotect", "insta protect", "instaprotect", "instagib protect", "instagibprotect":
+		return InstaProtect
+	case "hold":
+		return Hold
+	case "i hold", "ihold", "insta hold", "instahold", "instagib hold", "instagibhold":
+		return InstaHold
+	case "e ctf", "ectf", "effic ctf", "efficctf", "efficiency ctf", "efficiencyctf", "efficiency capture the flag":
+		return EfficCTF
+	case "e protect", "eprotect", "effic protect", "efficprotect", "efficiency protect", "efficiencyprotect":
+		return EfficProtect
+	case "e hold", "ehold", "effic hold", "effichold", "efficiency hold", "efficiencyhold":
+		return EfficHold
+	case "coll", "collect":
+		return Collect
+	case "i coll", "icoll", "i collect", "icollect", "insta collect", "instacollect", "instagib collect", "instagibcollect":
+		return InstaCollect
+	case "e coll", "ecoll", "e collect", "ecollect", "effic collect", "efficcollect", "efficiency collect", "efficiencycollect":
+		return EfficCollect
+	default:
+		return Unknown
 	}
 }
 
